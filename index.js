96b13d5b704e6e0fb67c91e2146f67553837da46
youssefAamer1710 <79087408+youssefAamer1710@users.noreply.github.com>
2023-12-01 00:59:12 +0200

    final commit

--- index.js
+++ index.js
@@ -11,19 +11,49 @@ const endorsmentsInDB = ref(database, "endorsements")
 
 const inputFieldEl = document.getElementById("input")
 const publishBtnEl = document.getElementById("publish-btn")
-const endorsementsListEl = document.getElementById("endorsements-list")
+const endorsementsListEl = document.getElementById("endorsments-list")
 
 publishBtnEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
     
-    // push("endorsmentsInDB", inputValue)
-    
-    console.log(inputValue)
+    push(endorsmentsInDB, inputValue)
     
     clearInputFieldEl()
 })
 
+onValue(endorsmentsInDB, function(snapshot) {
+    if (snapshot.exists()) {
+        let itemsArray = Object.entries(snapshot.val())
+        
+        clearEndorsementsListEl()
+        
+        for (let i = 0; i < itemsArray.length; i++) {
+            let currentItem = itemsArray[i]
+            let itemID = currentItem[0]
+            let itemValue = currentItem[1]
+            
+            appendItemToEndorsementsListEl(currentItem)
+        }
+    } else {
+        endorsementsListEl.innerHTML = `<p>No Endorsments... yet</p>`
+    }
+})
+
+function appendItemToEndorsementsListEl(item) {
+    let itemID = item[0]
+    let itemValue = item[1]
+    
+    let newEl = document.createElement("li")
+    
+    newEl.textContent = itemValue
+    
+    endorsementsListEl.append(newEl)
+}
 
 function clearInputFieldEl() {
     inputFieldEl.value = ""
+}
+
+function clearEndorsementsListEl() {
+    endorsementsListEl.innerHTML = ""
 }
